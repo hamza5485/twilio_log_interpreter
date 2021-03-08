4018b2c70be49f0334ea9671f9e98a075cb60e65
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-03-08 12:53:56 +0500

    current main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+from logger import Logger
+from typing import List, Dict
+from datetime import datetime
+import os
+import csv
+import pprint
+
+FILES = ["march_1", "march_2", "march_3", "march_4"]
+EXT = ".csv"
+CALL_LOG_DIR = "call_logs/"
+FLEX_LOG_DIR = "flex_logs/"
+INTERNAL_LOG_DIR = "internal_logs/"
+OUTBOUND = "+61352029217"
+
+log = Logger(os.path.basename(__file__))
+
+
+def get_file(file_name: str, dir: str) -> str:
+    """returns file name for provided date"""
+    if file_name in FILES:
+        return dir + file_name + EXT
+    else:
+        log.error(f"{file_name}{EXT} not found in {dir}")
+        raise Exception("404")
+
+
+def load_file(file_name: str) -> List[List]:
+    file_content = []
+    with open(file_name) as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader, None)
+        for row in reader:
+            file_content.append(row)
+    return file_content
+
+
+def internal_log_data(internal_log_file: str) -> None:
+    internal_log_data: List = load_file(internal_log_file)
+    print(f"total records in internal_log_file: {len(internal_log_data)}")
+
+
+def flex_data(flex_log_file: str):
+    flex_log_data: List = load_file(flex_log_file)
+    flex_log_counter: int = 0
+    for row in flex_log_data:
+        flex_log_counter += int(row[2])
+    print(f"total overflow count as per flex insights: {flex_log_counter}")
+
+
+def call_log_data(call_log_file: str):
+    call_log_data: List = load_file(call_log_file)
+    data: List = []
+    # overflow_count: int = 0
+    # non_overflow_count: int = 0
+    overflow_gaps: Dict = {}
+    for row in call_log_data:
+        if row[6] == OUTBOUND and row[7] == "Outgoing Dial":
+            data.append({
+                "incoming": {
+                    "to": "",
+                    "from": "",
+                    "direction": "",
+                    "start_time": ""
+                },
+                "outgoing": {
+                    "to": row[6],
+                    "from": row[5],
+                    "direction": row[7],
+                    "start_time": row[2]
+                },
+                "overflow_gap": "",
+                # "is_overflow": ""
+            })
+    for elem in data:
+        for row in call_log_data:
+            if row[7] == "Incoming" and row[5] == elem["outgoing"]["from"]:
+                elem["incoming"]["to"] = row[6]
+                elem["incoming"]["from"] = row[5]
+                elem["incoming"]["direction"] = row[7]
+                elem["incoming"]["start_time"] = row[2]
+                gap = (datetime.strptime(elem["outgoing"]["start_time"].replace("AEDT ", ''),
+                                         "%H:%M:%S %Y-%m-%d") - datetime.strptime(
+                    elem["incoming"]["start_time"].replace("AEDT ", ''), "%H:%M:%S %Y-%m-%d")).total_seconds()
+                elem["overflow_gap"] = gap
+    for e in data:
+        key = e["overflow_gap"]
+        if key not in overflow_gaps:
+            overflow_gaps[key] = 1
+        else:
+            overflow_gaps[key] += 1
+
+    print(f"total: {len(data)}")
+    pp = pprint.PrettyPrinter(indent=2)
+    pp.pprint(overflow_gaps)
+    # print(f"total overflown: {overflow_count}")
+    # print(f"total other: {non_overflow_count}")
+    # print(f"{overflow_count} + {non_overflow_count} = {non_overflow_count + overflow_count}")
+    # pp = pprint.PrettyPrinter(indent=2)
+    # for e in data:
+    #     if not e["is_overflow"]:
+    #         pp.pprint(e)
+
+
+if __name__ == '__main__':
+    for file in FILES:
+        print(f"*****_{file}_*****")
+        internal_log_file: str = get_file(file, INTERNAL_LOG_DIR)
+        call_log_file: str = get_file(file, CALL_LOG_DIR)
+        flex_log_file: str = get_file(file, FLEX_LOG_DIR)
+        internal_log_data(internal_log_file)
+        print("===================")
+        flex_data(flex_log_file)
+        print("===================")
+        call_log_data(call_log_file)
+        print("===================")
+
